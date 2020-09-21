6f376657cdfadfdabb3c8c0703f10d82fe3b4e99
Emullan04 <nallum.acire@gmail.com>
2020-09-21 17:52:10 -0500

    added file 20- testing

---
+++ Exercise 20/index.js
@@ -0,0 +1,24 @@
+let age = 20;
+let firstName = "Erica";
+let finishedCourse = false;
+let finalScore = null;
+let winningNumbers;
+
+
+console.log(age);
+console.log(firstName);
+console.log(finishedCourse);
+console.log(finalScore);
+console.log(winningNumbers);
+
+let age = String(20);
+let firstName = Boolean("Erica");
+let finishedCourse = Number(false);
+let finalScore = Number(null);
+let winningNumbers = Boolean;
+
+console.log(age);
+console.log(firstName);
+console.log(finishedCourse);
+console.log(finalScore);
+console.log(winningNumbers);
