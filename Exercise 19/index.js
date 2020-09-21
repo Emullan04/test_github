094282bb8d5c6d3f8227e483c691c5cf5adb2b8c
Emullan04 <nallum.acire@gmail.com>
2020-09-21 17:44:30 -0500

    added file 18 & 19

---
+++ Exercise 19/index.js
@@ -0,0 +1,18 @@
+const monthOfBirth = "August";
+const firstName = "Erica";
+const lastName = "Mullan";
+
+let city;
+let age;
+let nextCity;
+
+let city = "Winnipeg";
+let age = 29;
+let nextCity = city;
+
+console.log("Birth Month: ", monthOfBirth);
+console.log("First Name: ", firstName);
+console.log("Last Name: ", lastName);
+console.log("City: ", city);
+console.log("Age: ", age);
+console.log("Next City: ", nextCity);
